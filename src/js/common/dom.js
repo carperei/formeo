f32375639795cf9bca01632c5b86c28e99c3c5bf
carperei <70909170+carperei@users.noreply.github.com>
2024-10-09 15:10:28 +0100

    fix label radio-group

--- src/js/common/dom.js
+++ src/js/common/dom.js
@@ -173,7 +173,7 @@ class DOM {
     if (elem.config) {
       if (
         elem.config.label &&
-        elem.meta && elem.meta.group !== 'layout' &&
+        ((elem.meta && elem.meta.group !== 'layout') || !elem.meta) &&
         ((elem.config.label && tag !== 'button') || ['radio', 'checkbox'].includes(h.get(elem, 'attrs.type'))) &&
         !isPreview
       ) {
